ac62adc01ae03b9f51c67a10e1d4aae68156488b
David Haskins <davidhaskins@gmail.com>
2015-10-04 20:41:58 -0500

    Unit Tests

---
+++ productfinder/src/AppBundle/Tests/Models/ProductTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Tests\Models;
+
+use AppBundle\Entity\Product;
+
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    //test getter/setter for Name field
+    public function testUserName()
+    {
+        $expectedName = 'headphones';
+        $this->product->setName($expectedName);
+
+        $this->assertEquals($expectedName, $this->product->getName());
+    }
+
+    //this function runs before each test runs
+    public function setUp()
+    {
+        $this->product = new Product();
+    }
+
+}
--- productfinder/src/AppBundle/Tests/Models/UserTest.php
+++ productfinder/src/AppBundle/Tests/Models/UserTest.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 10/4/2015
- * Time: 8:24 PM
- */
+namespace AppBundle\Tests\Models;
+
+use AppBundle\Entity\User;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    //test getter/setter for userName field
+    public function testUserName()
+    {
+        $expectedUserName = 'sweng500user';
+        $this->user->setUserName($expectedUserName);
+
+        $this->assertEquals($expectedUserName, $this->user->getUserName());
+    }
+
+    //this function runs before each test runs
+    public function setUp()
+    {
+        $this->user = new User();
+    }
+
+}
