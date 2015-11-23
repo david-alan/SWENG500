dab91d91aa9d0123ba77ab51181d7f564c0c049c
David Haskins <davidhaskins@gmail.com>
2015-11-22 21:15:37 -0600

    harness to manually test publisher without the scrapers

---
+++ productfinder/src/AppBundle/Tests/Fixtures/sendJsonFixture.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Fixtures;
+
+$dir = 'vendor/';
+var_dump(scandir($dir));
+require 'vendor/autoload.php';
+
+use Guzzle\Http\Client;
+
+$postBody = file_get_contents(__DIR__ . '/testData.json');
+
+$client = new Client('http://54.67.71.88');
+$request = $client->post('/broadcast', [], $postBody);
+$response = $request->send();
+echo $response->getBody();
