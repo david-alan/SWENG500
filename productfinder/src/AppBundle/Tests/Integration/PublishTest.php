08a8e5f527636fbacef089d546640dddd0016516
David Haskins <davidhaskins@gmail.com>
2015-12-03 21:58:39 -0600

    clean up user class

---
+++ productfinder/src/AppBundle/Tests/Integration/PublishTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 11/28/2015
+ * Time: 4:13 PM
+ */
---
+++ productfinder/src/AppBundle/Tests/Models/WishlistTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 12/3/2015
+ * Time: 7:07 PM
+ */
